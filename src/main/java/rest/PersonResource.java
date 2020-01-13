185ec5ff1ca29ad0a9657b3b0fc66c9e63723c79
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2020-01-13 18:34:57 +0100

    As far as could get

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -3,7 +3,6 @@ package rest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dto.PersonDTO;
-import entities.Person;
 import utils.EMF_Creator;
 import facades.PersonFacade;
 import java.util.ArrayList;
